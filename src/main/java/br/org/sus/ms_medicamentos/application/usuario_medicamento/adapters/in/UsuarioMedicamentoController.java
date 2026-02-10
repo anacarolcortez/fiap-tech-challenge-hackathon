f6d1022d10bbb5497e90d1543977df3d82d317df
anacarolcortez <anacarolcortez@hotmail.com>
2026-02-10 07:35:19 -0300

    docs swagger

--- src/main/java/br/org/sus/ms_medicamentos/application/usuario_medicamento/adapters/in/UsuarioMedicamentoController.java
+++ src/main/java/br/org/sus/ms_medicamentos/application/usuario_medicamento/adapters/in/UsuarioMedicamentoController.java
@@ -4,6 +4,9 @@ import br.org.sus.ms_medicamentos.application.usuario_medicamento.adapters.in.dt
 import br.org.sus.ms_medicamentos.application.usuario_medicamento.adapters.in.dtos.response.UsuarioMedicamentoResponse;
 import br.org.sus.ms_medicamentos.application.usuario_medicamento.ports.in.CadastrarUsuarioMedicamentoPortIn;
 import br.org.sus.ms_medicamentos.application.usuario_medicamento.usecases.dto.UsuarioMedicamentoInput;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/usuario_medicamento")
+@Tag(name = "Vínculo Usuário-Medicamento", description = "Endpoints para gerenciar os medicamentos associados aos usuários")
 public class UsuarioMedicamentoController {
 
     private final CadastrarUsuarioMedicamentoPortIn useCase;
@@ -21,6 +25,9 @@ public class UsuarioMedicamentoController {
         this.useCase = useCase;
     }
 
+    @Operation(summary = "Cadastra vínculo", description = "Associa um medicamento a um usuário e define se as notificações estão ativas.")
+    @ApiResponse(responseCode = "201", description = "Vínculo criado com sucesso")
+    @ApiResponse(responseCode = "400", description = "Dados de entrada inválidos")
     @PostMapping
     public ResponseEntity<UsuarioMedicamentoResponse> cadastrar(@RequestBody UsuarioMedicamentoRequest request) {
 
--- src/main/java/br/org/sus/ms_medicamentos/application/usuario_medicamento/adapters/in/dtos/request/UsuarioMedicamentoRequest.java
+++ src/main/java/br/org/sus/ms_medicamentos/application/usuario_medicamento/adapters/in/dtos/request/UsuarioMedicamentoRequest.java
@@ -1,10 +1,17 @@
 package br.org.sus.ms_medicamentos.application.usuario_medicamento.adapters.in.dtos.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.UUID;
 
 public record UsuarioMedicamentoRequest(
+        @Schema(example = "123e4567-e89b-12d3-a456-426614174000")
         UUID usuarioId,
+
+        @Schema(example = "10")
         UUID medicamentoId,
+
+        @Schema(example = "true")
         boolean notificacaoAtiva
 ) {
 }
